2be32dbc8cee469400527ed87b7d8776f30e26c3
Pedro Guimarães <moisespedro15@gmail.com>
2022-08-10 14:54:19 -0300

    Adds script to update binary, uses xz compression

--- fetch.sh
+++ fetch.sh
@@ -4,8 +4,8 @@ INSTALL_DIR="/usr/local/bin"
 sudo mkdir -p "$INSTALL_DIR"
 
 echo "-- Fetching firebase binary..."
-sudo curl https://raw.githubusercontent.com/CampGladiator/firebase-binary-ci/main/firebase.tar.gz -L --progress-bar | tar -xz
-sudo mv firebase_bin "$INSTALL_DIR/firebase"
+sudo curl https://raw.githubusercontent.com/CampGladiator/firebase-binary-ci/main/firebase.tar.xz -L --progress-bar | tar -xvz
+sudo mv firebase "$INSTALL_DIR/"
 
 echo "-- Setting permissions on binary..."
 sudo chmod +rx "$INSTALL_DIR/firebase"
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "-- Downloading latest firebase binary..."
+curl -Lo ./firebase https://firebase.tools/bin/macos/latest
+
+echo "-- Compressing firebase binary..."
+XZ_OPT=-9e tar cJf firebase.tar.xz firebase
+
+echo "-- Deleting uncompressed binary..."
+rm firebase
+
+echo "-- Pushing new firebase binary..."
+git config --global user.name "Pedro Guimarães"
+git config --global user.email "moisespedro15@gmail.com"
+git add .
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push
+
+exit 0
